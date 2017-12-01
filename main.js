797719ea00170a152389e62f7b68bfd1619d6bd2
Matheus Pimenta <matheuscscp@gmail.com>
2017-12-01 12:22:56 -0200

    Improving bot

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const rand = require('random-seed').create(Date.now())
 
 // constants
 const botToken = process.env.BOT_TOKEN
-const chatId = process.env.CHAT_ID
+const chatIds = process.env.CHAT_IDS.split(' ').map(id => Number(id))
 const phrases = [
   'contrata os servicos da net virtua ai porra',
   'humano, a sua busca me cansa',
@@ -16,53 +16,59 @@ const phrases = [
 ]
 
 // globals
-let waitintInput = null
+const waitintInput = new Map()
+chatIds.forEach(id => waitintInput.set(id, new Set()))
 
 // random messages
 const getMessage = () => phrases[rand.intBetween(0, phrases.length - 1)]
 
 // bot
 const bot = new TelegramBot({ token: botToken, updates: { enabled: true } })
-const botMessage = (text) => bot.sendMessage({ chat_id: chatId, text })
+const botMessage = (chat_id, text) => bot.sendMessage({ chat_id, text })
 bot.on('update', (update) => {
-  if (!update.message || update.message.chat.id != chatId) return
-  if (waitintInput == update.message.from.id) {
-    waitintInput = null
-    getGif(update.message.text)
-  } else if (update.message.text == '/healthcheck@qqmerda_bot') {
-    botMessage('to vivao')
-  } else if (update.message.text == '/giphy@qqmerda_bot') {
+  if (!update.message || chatIds.indexOf(update.message.chat.id) < 0) return
+  const chat = update.message.chat.id
+  const user = update.message.from.id
+  const text = update.message.text
+  const s = waitintInput.get(chat)
+  if (s.has(user)) {
+    s.delete(user)
+    getGif(chat, text)
+  } else if (text == '/healthcheck@qqmerda_bot') {
+    botMessage(chat, 'to vivao nenem')
+  } else if (text == '/giphy@qqmerda_bot') {
     waitintInput = update.message.from.id
-    botMessage('q tag arrombado?')
+    s.add(user)
+    botMessage(chat, 'q tag arrombado?')
   }
 })
 
 // get random gif
-const getGif = (tag) => {
+const getGif = (chat, tag) => {
   console.log('fetching gif')
-  botMessage(getMessage())
+  botMessage(chat, getMessage())
   giphy.random(tag)
   .then((res) => {
     console.log('gif fetched')
-    sendGif(tag, res.data.image_url, 0)
+    sendGif(chat, tag, res.data.image_url, 0)
   })
   .catch((err) => console.error(err))
 }
 
 // send gif
-const sendGif = (tag, url, count) => {
+const sendGif = (chat_id, tag, url, count) => {
   if (count == 3) {
-    botMessage('sinto muito mas sua pesquisa eh idiota dms')
+    botMessage(chat_id, 'sinto muito mas sua pesquisa eh idiota d+')
     console.log('aborting')
     return
   }
   console.log('sending gif')
-  botMessage(getMessage())
+  botMessage(chat_id, getMessage())
   bot.sendVideo({
-    chat_id: chatId,
+    chat_id,
     caption: `ta aqui seu ${tag} seu bosta`,
     video: url
   })
   .then((data) => console.log('gif sent'))
-  .catch((err) => sendGif(tag, url, count + 1))
+  .catch((err) => sendGif(chat_id, tag, url, count + 1))
 }
